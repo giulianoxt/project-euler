bc49540dc926607124555f8672f5ef1092f8fc28
Giuliano Vilela <giulianoxt@gmail.com>
2011-05-22 11:20:37 -0300

    Problem 23: Use a set of abundant numbers to reduce time complexity.

--- src/p23.py
+++ src/p23.py
@@ -1,20 +1,20 @@
 from euler import *
-from itertools import count
+from itertools import count, ifilter
 
 def abundant(n):
   return sum(divisors(n)) > 2*n
 
 L = 28124
-abund = map(abundant, xrange(L))
+abund = set(ifilter(abundant, xrange(L)))
 
 res = 0
 for n in xrange(L):
   sum_2 = False
-  for a in count(1):
+  for a in abund:
     b = n-a
     if a > b:
       break
-    if abund[a] and abund[b]:
+    if b in abund:
       sum_2 = True
       break
   if not sum_2:
